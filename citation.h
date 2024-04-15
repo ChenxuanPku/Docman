574f478eab0b8a1a7c54bf5e15dad99ab9a037c2
ChenxuanPku <chenxuan@stu.pku.edu.cn>
2024-04-15 13:47:04 +0800

    modified:   citation.h

--- citation.h
+++ citation.h
@@ -5,6 +5,47 @@
 #include <string>
 
 class Citation {
-};
+private:
 
+
+public: 
+std::string Title; 
+virtual std::string getType() 
+  {
+     return "citation";
+  } 
+};
+class Book: Citation{
+private:
+std::string Author;
+std::string Publisher;
+std::string Year;
+public:
+std::string getType() 
+  {
+     return "book";
+  } 
+};
+class Article:public Citation{
+private:
+std::string Author;
+std::string Year;
+std::string Journal;
+std::string Volume;
+std::string Issue;
+public:
+std::string getType() 
+  {
+     return "article";
+  } 
+};
+class Webpage:public Citation{
+private:
+std::string Url;
+public:
+std::string getType() 
+  {
+     return "webpage";
+  } 
+};
 #endif
